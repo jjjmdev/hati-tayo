6f87471402b4e49389bf0c0fcae415c6d20ee98b
Joshua King <162310653+jjjmdev@users.noreply.github.com>
2026-02-16 09:55:00 +0800

    Add header title and description

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@trendyol/baklava'
+import { setIconPath } from '@trendyol/baklava'
+import '@trendyol/baklava/dist/themes/default.css'
+setIconPath('https://cdn.jsdelivr.net/npm/@trendyol/baklava-icons@latest/icons')
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
